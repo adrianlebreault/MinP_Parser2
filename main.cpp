4c14451d1e45da05f23246203a0bd61c0c0e505e
Adrian Lebreault <adrian.lebreault@gmail.com>
2021-03-25 16:58:18 -0400

    Added inital main only parser.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "y.tab.h"
+#include <fstream>
+
+
+using namespace std;
+
+extern vector<string> declares, uses, declerrors, typerrors;
+
+int main()
+{
+
+
+  
+
+    yyparse();
+
+    
+
+    return 0;
+}
